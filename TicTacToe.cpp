27559ee164dd066bc10bcc98ada20b7f4a7c0fb1
RingoB <zavadin143@gmail.com>
2016-04-10 21:58:52 +0400

    Update TicTacToe.cpp

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -4,19 +4,20 @@
 
 using namespace std;
 
-	char ryad1[10] = { '*','*', '*', '*', '*', '*', '*', '*', '*', '*' };
-	char ryad2[10] = { '*','*', '*', '*', '*', '*', '*', '*', '*', '*' };
-	char ryad3[10] = { '*','*', '*', '*', '*', '*', '*', '*', '*', '*' };
-	char ryad4[10] = { '*','*', '*', '*', '*', '*', '*', '*', '*', '*' };
-	char ryad5[10] = { '*','*', '*', '*', '*', '*', '*', '*', '*', '*' };
-	char ryad6[10] = { '*','*', '*', '*', '*', '*', '*', '*', '*', '*' };
-	char ryad7[10] = { '*','*', '*', '*', '*', '*', '*', '*', '*', '*' };
-	char ryad8[10] = { '*','*', '*', '*', '*', '*', '*', '*', '*', '*' };
-	char ryad9[10] = { '*','*', '*', '*', '*', '*', '*', '*', '*', '*' };
-	char ryad10[10] = { '*','*', '*', '*', '*', '*', '*', '*', '*', '*' };
-	int player = 1;
+char ryad1[10] = { '*','*', '*', '*', '*', '*', '*', '*', '*', '*' };
+char ryad2[10] = { '*','*', '*', '*', '*', '*', '*', '*', '*', '*' };
+char ryad3[10] = { '*','*', '*', '*', '*', '*', '*', '*', '*', '*' };
+char ryad4[10] = { '*','*', '*', '*', '*', '*', '*', '*', '*', '*' };
+char ryad5[10] = { '*','*', '*', '*', '*', '*', '*', '*', '*', '*' };
+char ryad6[10] = { '*','*', '*', '*', '*', '*', '*', '*', '*', '*' };
+char ryad7[10] = { '*','*', '*', '*', '*', '*', '*', '*', '*', '*' };
+char ryad8[10] = { '*','*', '*', '*', '*', '*', '*', '*', '*', '*' };
+char ryad9[10] = { '*','*', '*', '*', '*', '*', '*', '*', '*', '*' };
+char ryad10[10] = { '*','*', '*', '*', '*', '*', '*', '*', '*', '*' };
+int player = 1;
+int hodi = 0;
 
-int hod(int a, int b) {  //Ôóíêöèÿ, îòâå÷àþùàÿ çà õîäû.
+int hod(int a, int b) {  //Функция, отвечающая за ходы.
 	if (a == 1) {
 		if (ryad1[b - 1] != '*') {
 			return 1;
@@ -129,14 +130,14 @@ int hod(int a, int b) {  //Ôóíêöèÿ, îòâå÷àþùàÿ çà õîäû.
 	}
 	return 0;
 }
-int endgamecheck() { //Ïðîâåðêà êîìáèíàöèé
+int endgamecheck() { //Проверка комбинаций
 	int k = 0;
 	int s = 0;
 	int d = 0;
 	int n = 0;
 	for (int i = 0; i < 10; i++) {
 		if (k > 3) { return 1; }
-		if ((ryad1[i] == ryad2[i]) &&(ryad1[i] != '*')) { k++; }
+		if ((ryad1[i] == ryad2[i]) && (ryad1[i] != '*')) { k++; }
 		else { k = 0; }
 		if (k > 3) { return 1; }
 		if ((ryad2[i] == ryad3[i]) && (ryad2[i] != '*')) { k++; }
@@ -148,7 +149,7 @@ int endgamecheck() { //Ïðîâåðêà êîìáèíàöèé
 		if ((ryad4[i] == ryad5[i]) && (ryad4[i] != '*')) { k++; }
 		else { k = 0; }
 		if (k > 3) { return 1; }
-		if ((ryad5[i] == ryad6[i] )&& (ryad5[i] != '*')) { k++; }
+		if ((ryad5[i] == ryad6[i]) && (ryad5[i] != '*')) { k++; }
 		else { k = 0; }
 		if (k > 3) { return 1; }
 		if ((ryad6[i] == ryad7[i]) && (ryad6[i] != '*')) { k++; }
@@ -227,64 +228,75 @@ int endgamecheck() { //Ïðîâåðêà êîìáèíàöèé
 	for (int z = 0; z < 10; z++) {
 		int i = 0;
 		if (d > 3) { return 1; }
+		i = 0;
 		if (z < 1) {
 			if ((ryad1[i] == ryad2[i + 1]) && (ryad1[i] != '*')) { d++; }
 			else { d = 0; }
+			if (d > 3) { return 1; }
 			i++;
 		}
 		if (z < 2) {
 			if (d > 3) { return 1; }
 			if ((ryad2[i] == ryad3[i + 1]) && (ryad2[i] != '*')) { d++; }
 			else { d = 0; }
+			if (d > 3) { return 1; }
 			i++;
 		}
 		if (z < 3) {
 			if (d > 3) { return 1; }
 			if ((ryad3[i] == ryad4[i + 1]) && (ryad3[i] != '*')) { d++; }
 			else { d = 0; }
+			if (d > 3) { return 1; }
 			i++;
 		}
 		if (z < 4) {
 			if (d > 3) { return 1; }
 			if ((ryad4[i] == ryad5[i + 1]) && (ryad4[i] != '*')) { d++; }
 			else { d = 0; }
+			if (d > 3) { return 1; }
 			i++;
 		}
 		if (z < 5) {
 			if (d > 3) { return 1; }
 			if ((ryad5[i] == ryad6[i + 1]) && (ryad5[i] != '*')) { d++; }
 			else { d = 0; }
+			if (d > 3) { return 1; }
 			i++;
 		}
 		if (z < 6) {
 			if (d > 3) { return 1; }
 			if ((ryad6[i] == ryad7[i + 1]) && (ryad6[i] != '*')) { d++; }
 			else { d = 0; }
+			if (d > 3) { return 1; }
 			i++;
 		}
 		if (z < 7) {
 			if (d > 3) { return 1; }
 			if ((ryad7[i] == ryad8[i + 1]) && (ryad7[i] != '*')) { d++; }
 			else { d = 0; }
+			if (d > 3) { return 1; }
 			i++;
 		}
 		if (z < 8) {
 			if (d > 3) { return 1; }
 			if ((ryad8[i] == ryad9[i + 1]) && (ryad8[i] != '*')) { d++; }
 			else { d = 0; }
+			if (d > 3) { return 1; }
 			i++;
 		}
 		if (z < 9) {
 			if (d > 3) { return 1; }
 			if ((ryad9[i] == ryad10[i + 1]) && (ryad9[i] != '*')) { d++; }
 			else { d = 0; }
+			if (d > 3) { return 1; }
 			i++;
 		}
 		if (d > 3) { return 1; }
+
 	}
 
 	for (int z = 0; z < 10; z++) {
-		int i = 9;
+		int i = 10;
 		if (n > 3) { return 1; }
 		if (z < 1) {
 			if ((ryad1[i] == ryad2[i - 1]) && (ryad1[i] != '*')) { n++; }
@@ -343,13 +355,13 @@ int endgamecheck() { //Ïðîâåðêà êîìáèíàöèé
 	}
 	return 0;
 }
-void pole() { //Âûâîä ïîëÿ.
+void pole() { //Вывод поля.
 	for (int i = 1; i < 11; i++) {
 		cout << i << " ";
 	}
 	cout << endl;
 	for (int i = 0; i < 10; i++) {
-		cout << ryad1[i]<<" ";
+		cout << ryad1[i] << " ";
 	}
 	cout << "1";
 	cout << endl;
@@ -400,7 +412,7 @@ void pole() { //Âûâîä ïîëÿ.
 	cout << endl;
 	return;
 }
-int newgame() { //Îáíóëåíèå èãðû.
+int newgame() { //Обнуление игры.
 	for (int i = 0; i < 10; i++) {
 		ryad1[i] = '*';
 	}
@@ -435,57 +447,36 @@ int newgame() { //Îáíóëåíèå èãðû.
 	system("cls");
 	return 0;
 }
-int main() { //Ñîáñòâåííî ìýèí.
+
+int main() { //Собственно мэин.
 	setlocale(LC_ALL, "rus");
 newgame:
-	cout << "Êðåñòèêè-íîëèêè v1.0 (ñ) RingoBarbos" << endl;
+	cout << "Крестики-нолики v1.0 (с) RingoB" << endl;
 again:
 	pole();
 	int a, b;
-	int hodi = 0;
 	int check;
 	char otvet;
 retry:
-	cout << "Õîä èãðîêà" << player << ": ";
+	cout << "Ход игрока" << player << ": ";
 	cin >> a >> b;
-	if ((a > 10)||(b > 10) || (a < 1) || (b < 1)) {
-		cout << "ÎØÈÁÊÀ! ÂÛ ÂÂÅËÈ ÊÎÎÐÄÈÍÀÒÛ ÇÀ ÏÐÅÄÅËÀÌÈ ÏÎËß!" << endl;
+	if ((a > 10) || (b > 10) || (a < 1) || (b < 1)) {
+		cout << "ОШИБКА! ВЫ ВВЕЛИ КООРДИНАТЫ ЗА ПРЕДЕЛАМИ ПОЛЯ!" << endl;
 		goto retry;
 	}
-	check = hod(a,b);
+	check = hod(a, b);
 	if (check == 1) {
-		cout << "ÎØÈÁÊÀ! ÊËÅÒÊÀ ÇÀÍßÒÀ!" << endl;
+		cout << "ОШИБКА! КЛЕТКА ЗАНЯТА!" << endl;
 		goto retry;
 	}
 	hod(a, b);
-	int end = endgamecheck();
-	int draw=0;
 	hodi++;
-	if (hodi == 100) {
-		cout << "Íè÷üÿ!" << endl;
-		draw = 1;
-	}
-	if (draw == 1) {
-		cout << "Õîòèòå íà÷àòü çàíîâî? y/n?: ";
-zan:
-		cin >> otvet;
-		if (otvet == 'y') {
-			newgame();
-			goto newgame;
-		}
-		else if (otvet == 'n') {
-			return 0;
-		}
-		else {
-			cout << "ÎØÈÁÊÀ!" << endl;
-			goto zan;
-		}
-	}
+	int end = endgamecheck();
 	if (end == 1) {
 		pole();
-		cout << "Èãðîê " << player << " ïîáåäèë!" << endl;
-		cout << "Õîòèòå íà÷àòü çàíîâî? y/n?: ";
-zanovo:
+		cout << "Игрок " << player << " победил!" << endl;
+		cout << "Хотите начать заново? y/n?: ";
+	zanovo:
 		cin >> otvet;
 		if (otvet == 'y') {
 			newgame();
@@ -495,18 +486,33 @@ zanovo:
 			return 0;
 		}
 		else {
-			cout << "ÎØÈÁÊÀ!"<<endl;
+			cout << "ОШИБКА!" << endl;
 			goto zanovo;
 		}
 	}
-		if (player == 1) {
-			player = 2;
+	if (hodi == 100) {
+		pole();
+		cout << "Игрок " << player << " победил!" << endl;
+		cout << "Хотите начать заново? y/n?: ";
+	zan:
+		cin >> otvet;
+		if (otvet == 'y') {
+			newgame();
+			goto newgame;
+		}
+		else if (otvet == 'n') {
+			return 0;
 		}
 		else {
-			player = 1;
+			cout << "ОШИБКА!" << endl;
+			goto zan;
 		}
-		goto again;
+	}
+	if (player == 1) {
+		player = 2;
+	}
+	else {
+		player = 1;
+	}
+	goto again;
 }
-
-
-
